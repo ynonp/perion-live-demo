b37613489fd49f0da7d9df8eb6c594a1c96da5db
ynonp <ynonperek@gmail.com>
2018-07-16 11:26:23 +0300

    registration works

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,22 +1,19 @@
 class User < ActiveRecord::Base
   include BCrypt
-  validates_presence_of :password
+  validates_presence_of :password_hash
   validates_presence_of :name
 
   def password=(val)
+    return if val.blank?
     @password = Password.create(val)
     self.password_hash = @password    
   end
 
   def password
-    if password_hash.present?
-      @password ||= Password.new(password_hash)
-    else
-      ""
-    end
+    ''
   end
 
   def check_password(val)
-    password == val
+    Password.val(password_hash) == val
   end
 end
